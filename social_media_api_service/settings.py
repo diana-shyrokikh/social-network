2577b5a9debab8fd9238ab05239453a6a4199e65
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 20:52:46 +0300

    :wrench: Added Kyiv timezone and AUTH_USER_MODEL

--- social_media_api_service/settings.py
+++ social_media_api_service/settings.py
@@ -104,13 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "social_media.User"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
