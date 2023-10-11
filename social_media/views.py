220fb7ccb9e2d31a49d68b529f35a8bd9a5ab8a7
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 20:56:38 +0300

    :sparkles: Added user and post views

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from social_media.models import Post
+from social_media.serializers import (
+    UserSerializer,
+    PostSerializer
+)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
