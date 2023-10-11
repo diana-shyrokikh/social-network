0563f78f64e5b3815f792e1f4d2244e704f8ec77
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 21:00:54 +0300

    :wrench: Added UserLastRequestMiddleware middleware on settings.py

--- social_media_api_service/settings.py
+++ social_media_api_service/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'social_media.middleware.UserLastRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'social_media_api_service.urls'
