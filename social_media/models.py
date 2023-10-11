e97264ce4380cee590816b26ef1522f01b01a88c
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 20:50:59 +0300

    :sparkles: Added user and post models

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    last_request = models.DateTimeField(
+        default=datetime.now()
+    )
+    likes_count = models.IntegerField(default=0)
+
+
+class Post(models.Model):
+    text = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(null=True)
+    author = models.ForeignKey(
+        to=get_user_model(),
+        related_name="posts",
+        on_delete=models.CASCADE
+    )
+
