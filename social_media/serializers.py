93ca007556c604550976f688725166f6e718a75b
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 20:55:21 +0300

    :sparkles: Added user and post serializers

---
+++ social_media/serializers.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from social_media.models import Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+
+        fields = (
+            "id",
+            "last_login",
+            "username",
+            "password",
+            "last_request",
+            "likes_count",
+        )
+
+        read_only_fields = (
+            "id",
+            "last_login",
+            "last_request",
+            "likes_count",
+        )
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "text",
+            "author",
+        )
+        read_only_fields = (
+            "id",
+            "likes",
+            "dislikes",
+            "created_at",
+        )
