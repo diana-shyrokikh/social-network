ea68392f8a54de6f57f0adbfab837efbb7d455f1
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-11 21:27:33 +0300

    :sparkles: Updated PostViewSet

--- social_media/views.py
+++ social_media/views.py
@@ -22,3 +22,5 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
