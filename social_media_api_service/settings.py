ff7b6a5bf9075a8c8269d7f978584581027bd534
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-13 18:22:20 +0300

    :recycle: Refactored code

--- social_media_api_service/settings.py
+++ social_media_api_service/settings.py
@@ -123,6 +123,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
