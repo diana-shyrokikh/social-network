ddc1b1a31dadbeb51d07692053f25a48bfca36fd
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-13 21:10:57 +0300

    :sparkles: Added docs urls

--- social_network_api_service/urls.py
+++ social_network_api_service/urls.py
@@ -5,8 +5,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView,
+)
 
 urlpatterns = [
+    path(
+        "api/v1/doc/",
+        SpectacularAPIView.as_view(),
+        name="schema"
+    ),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
     path("api/v1/social_network/", include(
